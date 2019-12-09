a2c313726393b5e517aa351061ff254ce3d1dd60
Amgad Malik <amgadmalik@yahoo.com>
2019-12-09 02:55:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,4 +11,8 @@ class Pokemon
     @db = db
     @@all << self
   end
+  
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
 end
